a0cbea300ce6685d7402a43df05c8d71c74a69b7
kieutruc <kieubuithanhtruc2307@gmail.com>
2023-07-13 22:49:32 +0700

    bug:create-get-one-api-product refs:DTNF-5 tôi đã bug xong phần create-get-one-api-product

--- BE/modules/Product/Service/Product.js
+++ BE/modules/Product/Service/Product.js
@@ -1,5 +1,5 @@
 import Product from "../Model/Product.js";
 export const getOneproduct= async(req)=>{
-    const getOne = await product.findById(req.params.id)
+    const getOne = await Product.findById(req.params.id)
     return getOne
 }
